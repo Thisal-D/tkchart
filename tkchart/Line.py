a2e4fa01d1f034ab58c461b91d60c847f3a84b7b
Thisal <93121062+Thisal-D@users.noreply.github.com>
2023-04-22 03:47:39 +0530

    Update Line.py

--- tkchart/Line.py
+++ tkchart/Line.py
@@ -1,6 +1,7 @@
          
 class Line():
-   def __init__(self ,*arg ,master=None ,color="#ffffff" ,height=1):
+   def __init__(self ,*arg ,master=None ,color="#ffffff" ,height=1 ,
+                line_highlight=False ,line_highlight_size=0,line_highlight_color="#ffffff"):
       try :
          chart = arg[0]
       except:
@@ -11,9 +12,22 @@ class Line():
       self.y_end = 0
       self.x_end  = chart.chart_line_len*-1
       self.values = []
+      self.line_highlight = line_highlight
+      self.line_highlight_size = line_highlight_size
+      self.line_highlight_color = line_highlight_color
    
-   def configure(self , color=None ,height=None) :
+   def configure(self , color=None ,height=None ,line_highlight=None ,line_highlight_size=None ,line_highlight_color=None) :
       if color != None:
          self.line_color = color
       if height != None:
          self.line_height = height
+         
+      if line_highlight != None :
+         self.line_highlight = line_highlight
+        
+      if line_highlight_size != None:
+         self.line_highlight_size = line_highlight_size
+           
+      if line_highlight_color != None:
+         self.line_highlight_color = line_highlight_color
+ 

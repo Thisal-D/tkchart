c2ab8d8ee9b6334fcceb8162d583b7f5a45f17e2
Thisal <93121062+Thisal-D@users.noreply.github.com>
2024-01-12 03:15:14 +0530

    Update test3.py

--- examples/test3.py
+++ examples/test3.py
@@ -1,30 +1,48 @@
+import tkinter
+import customtkinter
 import tkchart
-import customtkinter as ctk
+
+import os
+import time
+
+root = customtkinter.CTk()
+root.configure(bg="#202020")
+
+root.minsize(1000,900)
+
+chart = tkchart.LineChart(root, width=950, height=250,y_data_max=1000,x_data_max=1000,x_values_decimals=5 ,y_data="GB" ,x_data="S" ,y_values_decimals=5,line_width=70)
+                        
+                        
+chart.place(x=0, y=0)
+
+chart.configure(x_data_color="#000000", y_data_color="#ff00ff",
+                x_values_color="#00ffff", y_values_color="#ff00ff",
+                y_labels_count=5, x_labels_count=5)
+
+
+line = tkchart.Line(master=chart, color="#ff0000", size=2)
+
+line2 = tkchart.Line(master=chart, color="#00ff00", size=2)
+
 import random
+data=[x for x in range(1,1001)]
+datas = [0,1000,0,1000,0,1000,0,1000,0,1000]
+index = 0
 
-root = ctk.CTk()
+def display():
+    global index
+    chart.show_data(line=line, data=[datas[index]])
+    index+=1
+    if index>= len(datas):
+        index =0
+    root.after(100, display)
+display()
 
-chart_1 = tkchart.LineChart(master=root 
-                            ,width=600 ,height=400 
-                            ,chart_fg='#050505'
-                            ,horizontal_bar_size=10 ,horizontal_bar_fg="#004400"
-                            ,vertical_bar_size=10 ,vertical_bar_fg="#004400"
-                            ,sections=True ,sections_fg="#004400" ,sections_count=5 
-                            ,max_value=150
-                            ,values_labels=True ,values_labels_count=10
-                            ,chart_line_len=80
-                            ,text_color='#004400' ,font=('arial',10,'bold') 
-                            ,left_space=10 ,right_space=10 ,bottom_space=40 ,top_space=40
-                            ,x_space=00 ,y_space=10)
-chart_1.pack()
 
-line_1 = tkchart.Line(master=chart_1 ,height=1 ,color='#444444')
-
-values = [x for x in range(121)]
-
-def loop():
-    chart_1.display(line=line_1 ,values=random.choices(values ,k=1))
-    root.after(100,loop)
-loop()
+def display2():
+    global speed
+    chart.show_data(line=line2, data=random.choices(data,k=1))
+    root.after(100, display2)
+display2()
 
 root.mainloop()

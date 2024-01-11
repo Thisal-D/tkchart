c2fef365f359b4480d3d57ea5ba724dfe7191553
Thisal <93121062+Thisal-D@users.noreply.github.com>
2024-01-12 03:12:20 +0530

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,26 +1,56 @@
-import tkchart
-import customtkinter as ctk
+import tkinter
+import tkchart as tkchart
+import random #for get random value
 
-root = ctk.CTk()
+#create root 
+root = tkinter.Tk()
+root.minsize(1200,500)
 
-chart_1 = tkchart.LineChart(master=root 
-                            ,width=1280 ,height=720 
-                            ,chart_fg='#050505'
-                            ,horizontal_bar_size=15
-                            ,vertical_bar_size=15 
-                            ,sections=True ,sections_fg="#202020" ,sections_count=5 
-                            ,max_value=150
-                            ,values_labels=True ,values_labels_count=10
-                            ,chart_line_len=80
-                            ,text_color='#808080' ,font=('arial',10,'bold') 
-                            ,left_space=100 ,right_space=100 ,bottom_space=40 ,top_space=40
-                            ,x_space=100 ,y_space=100)
-chart_1.pack()
+#create LineChart
+linechart = tkchart.LineChart(master=root, 
+                            width=1000, 
+                            height=400,
+                            bar_size=5,
+                            
+                            y_sections_count=5,
+                            x_sections_count=5,
+                            y_labels_count=5,
+                            x_labels_count=5,
+                            
+                            y_data="GB",
+                            x_data="S",
+                            x_data_max=60,
+                            y_data_max=1000,
+                            x_values_decimals=4,
+                            y_values_decimals=5,
+                            
+                            sections_color="#707070",
+                            y_values_color="#bbbbbb",
+                            x_values_color="#bbbbbb",
+                            x_data_color="#00ff00",
+                            y_data_color="#00ff00",
+                            bg_color="#202020",
+                            chart_color="#101010",
+                            bar_color="#cccccc",
+                            
+                            x_y_data_font = ("Arial", 15,"bold"),
+                            x_y_values_font = ("Arial", 10,"bold"),
+                            
+                            line_width=100)
+#place LineChart
+linechart.place(x=50 ,y=50)
 
-line_1 = tkchart.Line(master=chart_1 ,height=4 ,color='#909090')
 
-chart_1.display(line=line_1 ,values=[100,50,90,10,100,90,75,10,70,1,100,90,75,10,120])
+#Create Line
+line = tkchart.Line(master=linechart,
+                color="#cccccc",
+                size=3)
 
-
+data = [0,100,200,300,400,500,600,700,800,900,1000]
+def loop():
+    linechart.show_data(data=[random.choice(data)], line=line)
+    root.after(250,loop)
+#calling to loop
+loop()
 
 root.mainloop()

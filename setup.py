ae97f1b1ad2c7a2cafaa8746585c1f26e482fc2e
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2025-01-14 18:17:32 +0530

    add setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tkchart',  # Replace with your package name
+    version='2.1.5',  # Replace with your package version
+    author='Thisal-D',  # Replace with your name
+    author_email='khtdilmith@example.com',  # Replace with your email
+    description='Python library for creating live updating line charts in Tkinter GUIs.',
+    long_description=open('README.md').read(),  # Ensure you have a README.md file
+    long_description_content_type='text/markdown',
+    url='https://github.com/Thisal-D/tkchart',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+    install_requires=[],
+    include_package_data=True,
+)
